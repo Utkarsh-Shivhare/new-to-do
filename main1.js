0fa9ac11052d1d7bb16887787ca15de0b6c3aae5
Utkarsh-Shivhare <109977467+Utkarsh-Shivhare@users.noreply.github.com>
2022-09-09 08:36:30 -0700

    Update main1.js

--- main1.js
+++ main1.js
@@ -116,9 +116,12 @@ window.addEventListener('load', () => {
             pend_el.style.display="none";
             n=0;
             })
-
+             
             const all_el=document.querySelector("#all");
             all_el.addEventListener("click",function(){
+              all_el.style.boxShadow="0 15px 30px rgba(0, 0, 0, 0.3)";
+              filters_el.style.boxShadow="";
+              pending_el.style.boxShadow="";
               comp_el.style.display="none";
               list_el.style.display="block";
               pend_el.style.display="none";
@@ -126,6 +129,9 @@ window.addEventListener('load', () => {
 
             const pending_el=document.querySelector("#pending");
             pending_el.addEventListener("click",function(){
+              pending_el.style.boxShadow="0 15px 30px rgba(0, 0, 0, 0.3)";
+              filters_el.style.boxShadow="";
+              all_el.style.boxShadow="";
               pend_el.style.display="block";
               list_el.style.display="none";
               comp_el.style.display="none";
@@ -174,6 +180,7 @@ window.addEventListener('load', () => {
               task_remove_el2.classList.add("edit1")
               task_remove_el2.innerHTML="EDIT";
               
+              
               task_action_el2.appendChild(task_remove_el2);
               task_action_el2.appendChild(task_done_el2);
               
@@ -185,92 +192,40 @@ window.addEventListener('load', () => {
 
 
 
+             task_remove_el2.addEventListener("click",function(){
+              if(task_remove_el2.innerText=="EDIT"){
+                task_input_el2.removeAttribute("readonly");
+  
+            task_input_el2.focus();
+            task_remove_el2.innerText="SAVE";
+            task_input_el2.style.color="#91fc82";
 
 
+            }else{
+                task_input_el2.setAttribute("readonly",true);
+                task_remove_el2.innerText="EDIT"
+                task_input_el2.style.color="white";
+                task_input_el.value=task_input_el2.value;
+
+    }})
+             
+             task_done_el2.addEventListener("click",function(){
+              comp_el.appendChild(task_el1);
+              task_el2.remove();
+              task_action_el.appendChild(task_remove_el1);
+              task_el.appendChild(task_action_el);
+              task_done_el.innerHTML="<i class='fas fa-trash-restore-alt' style-'font-size:50px;></i>";
+              task_done_el.style.fontSize='25px';
+              task_done_el.style.color= '#2e97a5';
+              task_edit_el.remove();
+              task_input_el.style.textDecoration="line-through";
+              task_input_el.style.color="#ffffff3a";
+              list_el.style.display="none";
+              task_input_el1.value=task_input_el2.value;
+              task_input_el.style.boxShadow="10px 20px 30px blue";
+             })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //   function new_list(){
-        //     const task_el1 = document.createElement("div");
-        // task_el1.classList.add("comp");
-
-        // const task_content_el1 = document.createElement('div');
-        // task_content_el1.classList.add("content1");
-
-
-        // const task_input_el1 = document.createElement("input");
-        // task_input_el1.classList.add("text1");
-        // task_input_el1.type = 'text';
-        // task_input_el1.value = task;
-        // task_input_el1.setAttribute("readonly", "readonly");
-        // task_input_el1.style.color="#ffffff";
-        // task_input_el1.style.textDecoration="line-through";
-
-        //     task_content_el1.appendChild(task_input_el1);
-        //     task_el1.appendChild(task_content_el1);
-
-        //     const task_action_el1 = document.createElement("div");
-        //   task_action_el1.classList.add('action1');
-
-        //     const task_done_el1 = document.createElement("button");
-        //   task_done_el1.classList.add("img1");
-        //   task_done_el1.innerHTML="<i class='fas fa-trash-restore-alt' style-'font-size:50px;></i>";
-
-          
-        // //   const task_remove_el1=document.createElement("button");
-        // //   task_remove_el1.classList.add("remove1")
-        // //   task_remove_el1.innerHTML="<i class='fa fa-remove' style='font-size:25px;color:red'></i>";
-
-        //   task_action_el1.appendChild(task_done_el1);
-        //   task_action_el1.appendChild(task_remove_el);
-
-        //   task_el1.appendChild(task_action_el1);
-
-        // comp_el.appendChild(task_el1);
-        // comp_el.style.display="none";
-
-        //   console.log("done its going good");
-
-
-
-            // task_content_el.appendChild(task_input_el);
-            // task_action_el.appendChild(task_done_el);
-            // task_action_el.appendChild(task_remove_el);
-            // task_el.appendChild(task_content_el);
-            // task_el.appendChild(task_action_el);
-
-          // }
 
 
 
@@ -285,6 +240,7 @@ var n=0;
 
         task_edit_el.addEventListener('click',function(){
             if(task_edit_el.innerText=="EDIT"){
+
                 task_input_el.removeAttribute("readonly");
             task_input_el.focus();
             task_edit_el.innerText="SAVE";
@@ -315,6 +271,7 @@ var n=0;
             comp_el.appendChild(task_el1);
             comp_el.style.display="none";
             task_el2.remove();
+            task_input_el1.value=task_input_el.value;
 
 
             n=1;
@@ -335,14 +292,6 @@ var n=0;
         }
         })
         task_remove_el.addEventListener('click',function(){
-            // const task_input_el1 = document.createElement("input");
-            // task_input_el1.classList.add("text1");
-            // task_input_el1.type = 'text';
-            // task_input_el1.value = task;
-
-            // task_content_el1.appendChild(task_input_el1);
-            // task_el1.appendChild(task_content_el1);
-
             task_el.remove();
             task_el1.remove();
             console.log("no2")
@@ -362,6 +311,9 @@ var n=0;
         // })
         const filters_el=document.querySelector("#complete");
         filters_el.addEventListener('click',function(){
+          filters_el.style.boxShadow="0 15px 30px rgba(0, 0, 0, 0.3)";
+          all_el.style.boxShadow="";
+          pending_el.style.boxShadow="";
           comp_el.style.display="block";
           list_el.style.display="none";
           pend_el.style.display="none";
@@ -371,12 +323,5 @@ var n=0;
 
 
     })
-    // const filters_el=document.querySelector("#pending");
-    // filters_el.addEventListener('click',function(){
-    // //   comp_el.appendChild(task_el1);
-    //   list_el.style.display="none";
-    // })
-
-    // })
 
 })
